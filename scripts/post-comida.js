fc9c582174693ad376c6f85f11c6357b902ac2e1
clarilugm <105733197+clarilugm@users.noreply.github.com>
2026-06-22 17:38:04 -0300

    Cambios finales y quizz 2
    Se realizaron los cambios finales en el html y css. Además la creación del segundo quizz de postres.

--- scripts/post-comida.js
+++ scripts/post-comida.js
@@ -30,3 +30,49 @@ function corregirQuiz(){
   document.getElementById("resultadoQuiz").innerHTML =
     `Obtuviste ${puntos} de 3 respuestas correctas.<br>${mensaje}`;
 }
+
+
+//JUEGO DOS
+function corregirQuizPostre(){
+
+  const q1 = document.querySelector('input[name="q1-postre"]:checked');
+  const q2 = document.querySelector('input[name="q2-postre"]:checked');
+  const q3 = document.querySelector('input[name="q3-postre"]:checked');
+
+  if(!q1 || !q2 || !q3){
+    document.getElementById("resultadoQuiz").innerHTML =
+    "Respondé todas las preguntas.";
+    return;
+  }
+
+  let tiramisu = 0;
+  let gelato = 0;
+  let cannoli = 0;
+
+  const respuestas = [q1.value, q2.value, q3.value];
+
+  respuestas.forEach(r => {
+    if(r === "tiramisu") tiramisu++;
+    if(r === "gelato") gelato++;
+    if(r === "cannoli") cannoli++;
+  });
+
+  let resultado = "";
+  let mensaje = "";
+
+  if(tiramisu >= gelato && tiramisu >= cannoli){
+    resultado = "Tiramisú";
+    mensaje = "Café, intensidad y personalidad.";
+  }
+  else if(gelato >= cannoli){
+    resultado = "Gelato";
+    mensaje = "Fresco, dulce y equilibrado.";
+  }
+  else{
+    resultado = "Cannoli";
+    mensaje = "Tradición y dulzura, clásico.";
+  }
+
+  document.getElementById("resultadoQuizPostre").innerHTML =
+    `Tu postre es: <strong>${resultado}</strong><br>${mensaje}`;
+}
